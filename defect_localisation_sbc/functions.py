e8f335544c1f5c540ec596421d510abe5af8cf00
kkharbou <khadija1998kharbouch@gmail.com>
2025-02-06 17:28:36 +0000

    map evolution

--- defect_localisation_sbc/functions.py
+++ defect_localisation_sbc/functions.py
@@ -176,5 +176,7 @@ def get_marker_icon(defaut,gravite):
         return folium.Icon(icon="angles-down", prefix="fa", color=couleur)  # Icône de fissure (ou une autre icône)
     elif defaut.lower() == "concretion":
         return folium.Icon(icon="location-pin-lock", prefix="fa", color=couleur)  # Icône de fissure (ou une autre icône)
+    elif defaut.lower() == "autres":
+        return folium.Icon(icon="location-pin-lock", prefix="fa", color=couleur)  # Icône de fissure (ou une autre icône)
     else:
         return None
